afd13844025a521f00a1b1a3329cb1b34d2fa312
Nabin Kutu <nabin.kutu11@gmail.com>
2024-07-12 11:32:46 +0545

    protected component logic removed

--- src/components/ProtectedPage/index.tsx
+++ src/components/ProtectedPage/index.tsx
@@ -1,12 +1,12 @@
-import { Navigate, useLocation } from 'react-router-dom';
+// import { Navigate, useLocation } from 'react-router-dom';
 
 const ProtectedPage = ({ children }: { children: React.ReactNode }) => {
-  const location = useLocation();
-  const token = localStorage.getItem('token');
+  // const location = useLocation();
+  // const token = localStorage.getItem('token');
 
-  if (!token) {
-    return <Navigate to="/auth/signin" state={{ from: location }} replace />;
-  }
+  // if (!token) {
+  //   return <Navigate to="/auth/signin" state={{ from: location }} replace />;
+  // }
   return <>{children}</>;
 };
 
